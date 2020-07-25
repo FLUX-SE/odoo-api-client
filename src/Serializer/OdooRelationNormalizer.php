db75bb31f75f1310c78081e9e288bc405b1e66b7
Francis Hilaire <Prometee@users.noreply.github.com>
2020-07-25 10:06:48 +0200

    Fix null value and array of OdooRelation

--- src/Serializer/OdooRelationNormalizer.php
+++ src/Serializer/OdooRelationNormalizer.php
@@ -11,31 +11,20 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 final class OdooRelationNormalizer implements DenormalizerInterface, NormalizerInterface
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function denormalize($data, string $type, string $format = null, array $context = [])
-    {
-        if (false === is_array($data)) {
-            throw new InvalidArgumentException('The data should be an array !');
-        }
-
-        if (false === is_int($data[0] ?? null)) {
-            throw new InvalidArgumentException(
-                'The first element of the $data array should be an integer value !'
-            );
-        }
-
-        return new OdooRelation($data[0], $data[1] ?? null);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     public function supportsDenormalization($data, string $type, string $format = null): bool
     {
         if ($type !== OdooRelation::class) {
             return false;
+        }
+
+        // Case of null value (false in XMLRPC python API)
+        if (false === $data) {
+            return true;
+        }
+
+        // Case of many2many relation
+        if (is_int($data)) {
+            return true;
         }
 
         if (false === is_array($data)) {
@@ -53,24 +42,43 @@ final class OdooRelationNormalizer implements DenormalizerInterface, NormalizerI
         return true;
     }
 
-    public function normalize($object, string $format = null, array $context = [])
+    public function denormalize($data, string $type, string $format = null, array $context = [])
     {
-        if (false === $object instanceof OdooRelation) {
-            throw new InvalidArgumentException(sprintf(
-                'The object should be an instance of "%s" !',
-                OdooRelation::class
-            ));
+        if (false === $data) {
+            return null;
         }
 
-        return $object->getId();
+        if (is_int($data)) {
+            return new OdooRelation($data);
+        }
+
+        if (false === is_array($data)) {
+            throw new InvalidArgumentException('The data should be an array !');
+        }
+
+        if (false === is_int($data[0] ?? null)) {
+            throw new InvalidArgumentException(
+                'The first element of the $data array should be an integer value !'
+            );
+        }
+
+        return new OdooRelation($data[0], $data[1] ?? null);
     }
 
     public function supportsNormalization($data, string $format = null): bool
     {
-        if (false === $data instanceof OdooRelation) {
-            return false;
+        return $data instanceof OdooRelation;
+    }
+
+    public function normalize($object, string $format = null, array $context = [])
+    {
+        if ($object instanceof OdooRelation) {
+            return $object->getId();
         }
 
-        return true;
+        throw new InvalidArgumentException(sprintf(
+            'The object should be an instance of "%s" !',
+            OdooRelation::class
+        ));
     }
 }
