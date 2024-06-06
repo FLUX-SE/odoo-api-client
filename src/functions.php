f626fdbacdb3b08a0346403605840f675f06f041
Francis Hilaire <Prometee@users.noreply.github.com>
2024-06-06 15:17:21 +0200

    Add a comment about the serializer issue

--- src/functions.php
+++ src/functions.php
@@ -2,6 +2,12 @@
 
 declare(strict_types=1);
 
+/**
+ * Those functions are here to cover an issue on symfony/serializer
+ *
+ * @see https://github.com/symfony/symfony/issues/57334 for more details
+ */
+
 function is_false(mixed $value): bool
 {
     return is_bool($value) && false === (bool) $value;
